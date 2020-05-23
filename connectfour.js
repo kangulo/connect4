235ac143bfae94b24684629d9767bcfb5b84d58a
Kevin Angulo <kevin@local.com>
2020-05-23 11:57:24 -0400

    Draw Matrix and fill it out with circles

--- connectfour.js
+++ connectfour.js
@@ -2,8 +2,23 @@
 var canvas = document.getElementById("connectfour");
 var ctx = canvas.getContext('2d');
 // Matrix Dimensions
-var rows = 6;
-var cols = 7;
-ctx.fillStyle = "white";
-ctx.fillRect(10, 10, 80, 80);
+var num_of_rows = 6;
+var num_of_cols = 7;
+// Matrix
+var matrix = [];
+// Draw Matrix
+function drawMatrix(rows, cols) {
+    for (var x = 0; x < rows; x++) {
+        matrix[x] = [];
+        for (var y = 0; y < cols; y++) {
+            matrix[x][y] = 0;
+            ctx.fillStyle = "red";
+            ctx.beginPath();
+            ctx.arc(50 + (y * 100), 50 + (x * 100), 50, 0, 2 * Math.PI);
+            ctx.fill();
+        }
+    }
+    console.table(matrix);
+}
+drawMatrix(num_of_rows, num_of_cols);
 //# sourceMappingURL=connectfour.js.map
--- connectfour.ts
+++ connectfour.ts
@@ -3,9 +3,25 @@ const canvas = <HTMLCanvasElement>document.getElementById("connectfour");
 const ctx = canvas.getContext('2d');
 
 // Matrix Dimensions
-const rows = 6;
-const cols = 7;
+const num_of_rows = 6;
+const num_of_cols = 7;
 
-ctx.fillStyle = "white";
-ctx.fillRect(10, 10, 80, 80);
+// Matrix
+const matrix: number[][] = [];
 
+// Draw Matrix
+function drawMatrix(rows: number, cols: number): void {
+    for (let x = 0; x < rows; x++) {
+        matrix[x] = [];
+        for (let y = 0; y < cols; y++) {
+            matrix[x][y] = 0;
+            ctx.fillStyle = "red";
+            ctx.beginPath();
+            ctx.arc(50 + (y * 100), 50 + (x * 100), 50, 0, 2 * Math.PI);
+            ctx.fill();
+        }
+    }
+    console.table(matrix);
+}
+
+drawMatrix(num_of_rows, num_of_cols);
